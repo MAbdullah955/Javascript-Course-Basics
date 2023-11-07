69cf141ca75d0ec586ccc84f6b216368858a1a0d
MAbdullah955 <muhammad.abdullah@emumba.com>
2023-11-07 16:47:01 +0500

    Objects
    Object module completed. Moving on to String Output

--- pack.js
+++ pack.js
@@ -1,31 +1,81 @@
+// class Backpack {
+//     constructor (
+//         name,
+//         color,
+//         volume,
+//         zipOpen,
+//         strapLengthL,
+//         strapLengthR,
+//         dateAcquired
+//     )
+//     {
+//         this.name = name,
+//         this.color = color,
+//         this.volume = volume,
+//         this.zipOpen = zipOpen,
+//         this.strapLength = {
+//             Left: strapLengthL,
+//             Right: strapLengthR
+//         }
+//         this.dateAcquired = dateAcquired
+//     }
+
+//     toggleZip (lidStatus){
+//     this.zipOpen = zipStatus
+//     }   
+
+//     newStrapLength (lengthLeft, lengthRight){
+//     this.strapLength.Left = lengthLeft,
+//     this.strapLength.Right = lengthRight
+//     }
+
+//     backpackAge () {
+//         let now = new Date();
+//         let acquired = new Date(this.dateAcquired);
+//         let timeSpent = now - acquired;
+//         let daysSinceAcquired = Math.floor(timeSpent / (1000 * 3600 * 24));
+//         return daysSinceAcquired;
+//     }
+// }
+
+// export default Backpack;
+
 class Backpack {
-    constructor (
-        name,
-        color,
-        volume,
-        zipOpen,
-        strapLengthL,
-        strapLengthR
-    )
-    {
-        this.name = name,
-        this.color = color,
-        this.volume = volume,
-        this.zipOpen = zipOpen,
-        this.strapLength = {
-            Left: strapLengthL,
-            Right: strapLengthR
-        }
+    constructor(
+      name,
+      volume,
+      color,
+      pocketNum,
+      strapLengthL,
+      strapLengthR,
+      lidOpen,
+      dateAcquired
+    ) {
+      this.name = name;
+      this.volume = volume;
+      this.color = color;
+      this.pocketNum = pocketNum;
+      this.strapLength = {
+        left: strapLengthL,
+        right: strapLengthR,
+      };
+      this.lidOpen = lidOpen;
+      this.dateAcquired = dateAcquired;
     }
-
-    toggleZip (lidStatus){
-    this.ZipOpen = zipStatus
+    toggleLid(lidStatus) {
+      this.lidOpen = lidStatus;
     }
-
-    newStrapLength (lengthLeft, lengthRight){
-    this.strapLength.Left = lengthLeft,
-    this.strapLength.Right = lengthRight
+    newStrapLength(lengthLeft, lengthRight) {
+      this.strapLength.left = lengthLeft;
+      this.strapLength.right = lengthRight;
     }
-}
-
-export default Backpack;
+    backpackAge() {
+      let now = new Date();
+      let acquired = new Date(this.dateAcquired);
+      let elapsed = now - acquired;
+      let daysSinceAcquired = Math.floor(elapsed / (1000 * 3600 * 24));
+      return daysSinceAcquired;
+    }
+  }
+  
+  export default Backpack;
--- script.js
+++ script.js
@@ -1,15 +1,34 @@
+// import Backpack from "./pack.js";
+
+// const newPack = new Backpack(
+//     "newBag",
+//     "Black",
+//     30,
+//     false,
+//     15,
+//     17,
+//     "June 10, 2020 Wednesday"
+// );
+
+// console.log(newPack);
+// console.log(newPack.dateAcquired);
+// console.log(newPack.backpackAge());
+
+// export default newPack;
+
 import Backpack from "./pack.js";
 
-const newPack = new Backpack(
-    "newBag",
-    "Black",
-    30,
-    false,
-    15,
-    17
+const everydayPack = new Backpack(
+  "Everyday Backpack",
+  30,
+  "grey",
+  15,
+  26,
+  26,
+  true,
+  "December 5, 2018 15:00:00 PST"
 );
 
-console.log(newPack)
-console.log(newPack.strapLength)
-
-export default newPack;
+console.log("The everydayPack object:", everydayPack);
+console.log("Date acquired:", everydayPack.dateAcquired);
+console.log("Date acquired:", everydayPack.backpackAge());
