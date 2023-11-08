492c5a9d19d98439800a25e3560dae5e2612c09d
MAbdullah955 <muhammad.abdullah@emumba.com>
2023-11-08 17:26:46 +0500

    8/11/2023 Update

--- script.js
+++ script.js
@@ -1,51 +1,35 @@
-// import Backpack from "./pack.js";
+import Backpack from "./pack.js"
 
-// const newPack = new Backpack(
-//     "newBag",
-//     "Black",
-//     30,
-//     false,
-//     15,
-//     17,
-//     "June 10, 2020 Wednesday"
-// );
-
-// console.log(newPack);
-// console.log(newPack.dateAcquired);
-// console.log(newPack.backpackAge());
-
-// export default newPack;
-
-import Backpack from "./pack.js";
-
-const content = 
-<main>
-    <article>
-        <h1>Title</h1>
-        <ul>
-            <li>Name: ${everydayPack.name}</li>
-            <li>Volume: ${everydayPack.volume}</li>
-            <li>Color: ${everydayPack.color}</li>
-            <li>PocketNum: ${everydayPack.pocketNum}</li>
-            <li>StrapLengthL: ${everydayPack.strapLengthL}</li>
-            <li>StrapLengthR: ${everydayPack.strapLengthR}</li>
-            <li>LidOpen: ${everydayPack.lidOpen}</li>
-            <li>DateAcquired: ${everydayPack.dateAcquired}</li>
-        </ul>
-    </article>
-</main>
-
-const everydayPack = new Backpack(
+const newPack = new Backpack(
   "Everyday Backpack",
   30,
   "grey",
   15,
   26,
   26,
-  true,
-  "December 5, 2018 15:00:00 PST"
+  false,
+  "December 5, 2016 15:00:00 PST",
+  "../assets/images/everyday.svg"
 );
 
-console.log("The everydayPack object:", everydayPack);
-console.log("Date acquired:", everydayPack.dateAcquired);
-console.log("Date acquired:", everydayPack.backpackAge());
+const main = document.querySelector(".maincontent");
+
+const content = `
+  <article class="backpack" id="everyday">
+  <figure class="backpack__image">
+  <img src=${newPack.image} alt="" />
+</figure>
+<h1 class="backpack__name">${newPack.name}</h1>
+<ul class="backpack__features">
+  <li class="backpack__volume">Volume:<span> ${newPack.volume}l</span></li>
+  <li class="backpack__color">Color:<span> ${newPack.color}</span></li>
+  <li class="backpack__age">Age:<span> ${newPack.backpackAge()} days old</span></li>
+  <li class="backpack__pockets">Number of pockets:<span> ${newPack.pocketNum}</span></li>
+  <li class="backpack__strap">Left strap length:<span> ${newPack.strapLength.left} inches</span></li>
+  <li class="backpack__strap">Right strap length:<span> ${newPack.strapLength.right} inches</span></li>
+  <li class="backpack__lid">Lid status:<span> ${newPack.lidOpen}</span></li>
+</ul>
+  </article>
+`;
+
+main.innerHTML = content;
